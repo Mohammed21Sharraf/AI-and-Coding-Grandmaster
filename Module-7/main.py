bc17e5b378e10c591456899bcd7160eb81dc952d
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-01-31 17:42:32 +0600

    Double Check Number

--- Module-7/main.py
+++ Module-7/main.py
@@ -1,19 +1,11 @@
-height = float(input("Enter your height in cm: "))
-weight = float(input("Enter your weight in kg: "))
+num = int(input("Enter number to check :"))
 
-BMI = weight / (height/100)**2
+if num>50:
+	print("Number is greater than 50")
+	if num%2==0:
+		print("And it is even too")
+	else:
+		print("And it is odd")
 
-print("Your BMI is", BMI)
-
-if BMI <= 18.4:
-    print("You are underweight.")
-elif BMI <= 24.9:
-    print("You are healthy.")
-elif BMI <= 29.9:
-    print("You are over weight.")
-elif BMI <= 34.9:
-    print("You are severely over weight.")
-elif BMI <= 39.9:
-    print("You are obese.")
 else:
-    print("You are severely obese.")
+	print("Number is less than 50")
