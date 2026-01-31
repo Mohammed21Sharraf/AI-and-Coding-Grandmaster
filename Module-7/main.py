ea5e57c4eeb6c6d25f997690f7c93f804fdd1e65
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-01-31 17:49:54 +0600

    Star Pattern

--- Module-7/main.py
+++ Module-7/main.py
@@ -1,5 +1,11 @@
-# Python program to print the multiplication table of 23 from 1 to 10
-# Loop from 1 to 10
-for i in range(1, 11):
-    # Print the multiplication of 23 by i
-    print(f"23 x {i} = {23 * i}")
+# Python program to print a star pattern based on the number of rows specified by the user
+# Get the number of rows from user
+n = int(input("Enter the number of rows: "))
+# Outer loop for each row
+for i in range(1, n+1):
+    # Inner loop for each column in the row
+    for j in range(i):
+        # Print star, end with space instead of new line
+        print('*', end=' ')
+    # After each row, print a new line
+    print()
