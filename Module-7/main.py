8fc1ecf147bf3577fa5dcb9298b3cbb662add4c0
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-01-31 17:49:26 +0600

    Table of 23

--- Module-7/main.py
+++ Module-7/main.py
@@ -1,12 +1,5 @@
-import datetime  
-    
-# using now() to get current time  
-current_time = datetime.datetime.now()  
-    
-# Printing value of now.  
-print ("Time now at greenwich meridian is : ", end = "")   
-print (current_time)
-
-# print calendar of year 2021
-import calendar
-print("\n", calendar.calendar(2026))
+# Python program to print the multiplication table of 23 from 1 to 10
+# Loop from 1 to 10
+for i in range(1, 11):
+    # Print the multiplication of 23 by i
+    print(f"23 x {i} = {23 * i}")
