7f1494b13fd9f8cb1a14087f50176b9e54e9039e
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-01-31 18:03:32 +0600

    Star

--- Module-7/main.py
+++ Module-7/main.py
@@ -1,22 +1,31 @@
 import turtle
 
-# creating canvas
-# You can combine these:
-sc = turtle.Screen()
-sc.bgcolor("Orange")
-sc.setup(400, 300)
-
-turtle.title("Welcome to Turtle Window")
-
-# turtle object creation
+turtle.Screen().bgcolor("Orange")
 board = turtle.Turtle()
+ 
+# first triangle for star
+board.forward(100) # draw base
+ 
+board.left(120)
+board.forward(100)
+ 
+board.left(120)
+board.forward(100)
+ 
+board.penup()
+board.right(150)
+board.forward(50)
+ 
+# second triangle for star
+board.pendown()
+board.right(90)
+board.forward(100)
+ 
+board.right(120)
+board.forward(100)
+ 
+board.right(120)
+board.forward(100)
+ 
 
-# creating a square
-for i in range(4):
-    board.forward(100)
-    board.left(90)
-    # Note: In Python, 'i' increments automatically in a range loop, 
-    # so 'i = i + 1' isn't needed here.
-
-# THE MISSING LINE:
 turtle.done()
