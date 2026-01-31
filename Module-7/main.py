98ea1a154b3a94ffd8a0b5124b2701a896cda6b7
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-01-31 18:04:32 +0600

    Rainbow Spiral

--- Module-7/main.py
+++ Module-7/main.py
@@ -1,31 +1,19 @@
-import turtle
-
-turtle.Screen().bgcolor("Orange")
-board = turtle.Turtle()
- 
-# first triangle for star
-board.forward(100) # draw base
- 
-board.left(120)
-board.forward(100)
- 
-board.left(120)
-board.forward(100)
- 
-board.penup()
-board.right(150)
-board.forward(50)
- 
-# second triangle for star
-board.pendown()
-board.right(90)
-board.forward(100)
- 
-board.right(120)
-board.forward(100)
- 
-board.right(120)
-board.forward(100)
- 
-
-turtle.done()
+import turtle 
+t = turtle.Turtle()
+s = turtle.Screen()
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow'] 
+s.bgcolor('black') 
+t.speed('fastest')
+t.hideturtle()
+while True:
+  for x in range(200): 
+    t.pencolor(colors[x%len(colors)])
+    t.width(x/100 + 1)
+    t.forward(x) 
+    t.left(59)
+  t.right(239)  
+  for x in range(200, 0, -1): 
+    t.pencolor('black') 
+    t.width(x/100 + 7)
+    t.forward(x) 
+    t.right(59) 
