ec07a005cc26cbc91a8917d314266c15f9f7fde2
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 10:57:45 +0600

    Append Content

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,18 +1,36 @@
-# Program to count number of lines in this file
-# Opening a file
-file = open("Codingal.txt","r")
-Counter = 0
+# Program to append contents of file in another file
 
-# Reading from file
-Content = file.read()
-# splitting content into lines
-# and storing them in a list
-CoList = Content.split("\n")
+# entering the file names
+firstfile = input("Enter the name of first file ")
+secondfile = input("Enter the name of second file ")
 
-for i in CoList:
-	if i:
-		Counter += 1
-		
-print("This is the number of lines in the file")
-print(Counter)
+# opening both files in read only mode to read initial contents
+f1 = open(firstfile, 'r')
+f2 = open(secondfile, 'r')
 
+# printing the contens of the file before appending
+print('content of first file before appending -\n', f1.read())
+print('content of second file before appending - \n', f2.read())
+
+# closing the files
+f1.close()
+f2.close()
+
+# opening first file in append mode and second file in read mode
+f1 = open(firstfile, 'a+')
+f2 = open(secondfile, 'r')
+
+# appending the contents of the second file to the first file
+f1.write(f2.read())
+
+# relocating the cursor of the files at the beginning
+f1.seek(0)
+f2.seek(0)
+
+# printing the contents of the files after appendng
+print('content of first file after appending - \n', f1.read())
+print('content of second file after appending - \n', f2.read())
+
+# closing the files
+f1.close()
+f2.close()
