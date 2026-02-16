a32ec5750f644bebb7c55e0d936c03fea87736c4
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 11:19:01 +0600

    Let's top a window

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,22 +1,27 @@
 # Import necessary libraries
 from tkinter import *
-from tkinter import messagebox
-
-# Setup Tkinter Window
+ 
+# Setting up Main Window
 root = Tk()
-root.geometry("200x200")
+root.geometry("400x300")
+root.title("main")
 
-# Function for Displaying Warning Message
-# This will be called once the button is clicked
-# messagebox.showwarning("Window Name", "Text to be displayed")
-def msg():
-	messagebox.showwarning("Alert", "Stop! Virus Found.")
+# Function to open New (Top Level) Window
+def topwin():
+	# Setting up Top Window
+	top = Toplevel()
+	top.geometry("180x100")
+	top.title("toplevel")
+	# Adding a label widget to Top Window
+	l2 = Label(top, text = "This is toplevel window")
+	l2.pack()
 
-# Adding Button Widget to Window
-button = Button(root, text="Scan for Virus", command=msg)
-button.place(x=40, y=80)
+	top.mainloop()
 
-# Entering main event loop
-root.mainloop()
+# Adding a label and button Widget to Root (Main) Window
+l = Label(root, text = "This is root window")
+btn = Button(root, text = "Click here to open another window", command = topwin)
 
-
+# Arranging widgets
+l.pack()
+btn.pack()
