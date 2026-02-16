9ed2bb4f387dcfe57ee1644765f932db5216d482
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 10:52:46 +0600

    Lines in File

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,20 +1,18 @@
-# open the file in read mode
-file_read = open('Codingal.txt','r')
-print("File in Read Mode -")
-print(file_read.read())
-file_read.close()
+# Program to count number of lines in this file
+# Opening a file
+file = open("Codingal.txt","r")
+Counter = 0
 
-# open the file in write mode
-file_write = open('Codingal.txt', 'w')
-# write in the file 
-file_write.write(" File in write mode ....")
-file_write.write("Hi! I am Penguin. I am 1 yr. old ")
-file_write.close()
+# Reading from file
+Content = file.read()
+# splitting content into lines
+# and storing them in a list
+CoList = Content.split("\n")
 
-# open the file in append mode
-file_append = open('Codingal.txt', 'a')
-# append in the file 
-file_append.write("\n File in append mode ....")
-file_append.write("Hi! I am Penguin. I am 1 yr. old")
-file_append.close()
+for i in CoList:
+	if i:
+		Counter += 1
+		
+print("This is the number of lines in the file")
+print(Counter)
 
