22a67eeacdfc95d8c7e12d632e6c422cd7225ff0
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 09:55:59 +0600

    Set Union

--- Module-8/main.py
+++ Module-8/main.py
@@ -1,15 +1,8 @@
-my_set = {1,2,2,3,4,4,4}
-print("Set :", my_set)
-
-my_set.add(5)
-print("Updated Set:", my_set)
-
-set1 = my_set
-set2 = {2,4,4,6}
-
-print("\nSet 1", set1)
-print("Set 2", set2)
-print("Difference")
-print(set1.difference(set2))
-print("Symmeteric Difference")
-print(set1.symmetric_difference(set2))
+setc1 = {"green", "blue"}
+setc2 = {"blue", "yellow"}
+print("Original sets:")
+print(setc1)
+print(setc2)
+setc = setc1.union(setc2)
+print("\nUnion of above sets:")
+print(setc)
