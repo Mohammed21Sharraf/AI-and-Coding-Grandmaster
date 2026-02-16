a8fa3eac4d69a05fa82374e46974ceeae1023926
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 11:07:30 +0600

    Duplicate Lines

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,23 +1,26 @@
-#create a new file
-new_file = open('New_File.txt', 'x')
-new_file.close()
+# Program to eliminate repeated lines from a file
 
-#check if a file exists 
-import os
-print("Checking if my_file exists or not....")
-if os.path.exists("my_file.txt"):
-  os.remove("my_file.txt")
-else:
-  print("The file does not exist")
+# creating the output file
+outputFile = open('UpdatedFile.txt', "w")
 
-#create a new if it doesn't
-my_file = open("my_file.txt", "w")
-my_file.write("Hi! I am Penguin and I am 1 yr old.")
-my_file.close()
+# reading the input file
+inputFile = open('Repeated.txt', "r")
 
-#delete file named codingal
-os.remove('Codingal.txt')
+# holds lines already seen
+lines_seen_so_far = set()
+print("Eliminating duplicate lines....")
+# iterating each line in the file
+for line in inputFile:
 
-#delete the folder
-os.rmdir('Folder')
+	# checking if line is unique
+	if line not in lines_seen_so_far:
 
+		# write unique lines in output file
+		outputFile.write(line)
+
+		# adds unique lines to lines_seen_so_far
+		lines_seen_so_far.add(line)		
+
+# closing the file
+inputFile.close()
+outputFile.close()
