7fd53585269ab9374eddb1ec923095dc3f838eef
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 11:10:53 +0600

    Tkinter -2

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,21 +1,16 @@
-from tkinter import *
+import tkinter as tk
 
-window = Tk()
-window.title('Tkinter Sample Window')
-window.geometry('300x300')
+window = tk.Tk()
 
-greeting = Label(text="Hello User", fg='black', bg='white')
-button = Button(text="Click me", bg='black', fg='white')
-entry = Entry(fg="yellow", bg="blue", width=50)
-greeting.pack()
-button.pack()
-entry.pack()
+for i in range(3):
+    for j in range(3):
+        frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j, padx=5, pady=5)
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack()
 
-frame = Frame(master=window, relief=RAISED, borderwidth=5)
-frame.pack()
-label = Label(master=frame, text='Sample Frame')
-label.pack()
-
-textbox = Text(fg='green', bg='yellow')
-textbox.pack()
-
+window.mainloop()
