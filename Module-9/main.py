9c3db39ffd9141cbb3029cd231313c047d17e7c4
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 11:23:54 +0600

    Denomination Calculator

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,27 +1,62 @@
-# Import necessary libraries
 from tkinter import *
- 
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
 # Setting up Main Window
 root = Tk()
-root.geometry("400x300")
-root.title("main")
+root.title('Denomination Counter')
+root.configure(bg='light blue')
+root.geometry('650x400')
 
-# Function to open New (Top Level) Window
+# Adding Image and Labels in the Main Window
+upload = Image.open("app_img.jpg")
+# Resize the image using resize() method
+upload = upload.resize((300, 300))
+image = ImageTk.PhotoImage(upload)
+label = Label(root, image=image, bg='light blue')
+label.place(x=180, y=20)
+
+label1 = Label(root,
+    text="Hey User! Welcome to Denomination Counter Application.",
+    bg='light blue')
+label1.place(relx=0.5, y=340, anchor=CENTER)
+
+# Function to display a messagebox and proceed if OK is clicked
+def msg():
+    MsgBox = messagebox.showinfo(
+    "Alert", "Do you want to calculate the denomination count?")
+    if MsgBox == 'ok':
+        topwin()
+
+# Adding Buttons to the main window
+button1 = Button(root,
+    text="Let's get started!",
+    command=msg,
+    bg='brown',
+    fg='white')
+button1.place(x=260, y=360)
+
 def topwin():
-	# Setting up Top Window
-	top = Toplevel()
-	top.geometry("180x100")
-	top.title("toplevel")
-	# Adding a label widget to Top Window
-	l2 = Label(top, text = "This is toplevel window")
-	l2.pack()
+    # Top Window
+    top = Toplevel(root)
+    top.title("Currency Denomination Counter")
+    top.configure(bg='grey')
+    top.geometry('600x400')
 
-	top.mainloop()
+    # Centering Widgets in the Top Window
+    label.place(x=230, y=50)
+    entry.place(x=200, y=80)
+    btn.place(x=240, y=120)
+    lbl.place(x=140, y=170)
 
-# Adding a label and button Widget to Root (Main) Window
-l = Label(root, text = "This is root window")
-btn = Button(root, text = "Click here to open another window", command = topwin)
+    l1.place(x=180, y=200)
+    l2.place(x=180, y=230)
+    l3.place(x=180, y=260)
 
-# Arranging widgets
-l.pack()
-btn.pack()
+    t1.place(x=270, y=200)
+    t2.place(x=270, y=230)
+    t3.place(x=270, y=260)
+
+    top.mainloop()
+
+root.mainloop()
