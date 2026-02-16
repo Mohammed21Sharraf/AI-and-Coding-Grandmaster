55927a567d5c18d80aa97ad35ea15ea5447d34d2
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 11:01:39 +0600

    Remove Lines

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,20 +1,25 @@
-# open the file in read mode
-file_read = open('Codingal.txt','r')
-print("File in Read Mode -")
-print(file_read.read())
-file_read.close()
+# Program to remove lines starting with any prefix
 
-# open the file in write mode
-file_write = open('Codingal.txt', 'w')
-# write in the file 
-file_write.write(" File in write mode ....")
-file_write.write("Hi! I am Penguin. I am 1 yr. old ")
-file_write.close()
+file1 = open('Codingal.txt',
+			'r')
+file2 = open('CodingalUpdated.txt',
+			'w')
 
-# open the file in append mode
-file_append = open('Codingal.txt', 'a')
-# append in the file 
-file_append.write("\n File in append mode ....")
-file_append.write("Hi! I am Penguin. I am 1 yr. old")
-file_append.close()
+# reading each line from original
+# text file
+for line in file1.readlines():
+	
+	# reading all lines that do not
+	# begin with "Coding"
+	if not (line.startswith('Coding')):
+		
+		# printing those lines
+		print(line)
+		
+		# storing only those lines that
+		# do not begin with "Coding"
+		file2.write(line)
 
+# close and save the files
+file2.close()
+file1.close()
