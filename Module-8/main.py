377f6ebfd184131b117f4d34ee44efe79b706a0f
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 10:01:27 +0600

    Class Student-2

--- Module-8/main.py
+++ Module-8/main.py
@@ -1,5 +1,14 @@
 class student:
 	grade = 10
-	print("Hi I am a student of grade", grade)
+	name = "Penguin"
+	
+	def introduction(self):
+		print("Hi I am a student")
+
+	def details(self):
+		print("My name is", self.name)
+		print("I study in Grade", self.grade)
 
 ob = student()
+ob.introduction()
+ob.details()
