dc85dbf81edd251645fdebdaed269e0cbeea7071
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 11:05:45 +0600

    File Handling Operations Part-2

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,14 +1,23 @@
-# write in file using with()function
-with open('Codingal1.txt', 'w') as file:
-  file.write("Hi! I am Penguin and I am 1 yr old.")
-file.close()
+#create a new file
+new_file = open('New_File.txt', 'x')
+new_file.close()
 
-# split file into words
-with open('Codingal1.txt', 'r') as file:
-  data = file.readlines()
-  print("Words in this file are....")
-  for line in data:
-    word = line.split()
-    print (word)
-file.close()
+#check if a file exists 
+import os
+print("Checking if my_file exists or not....")
+if os.path.exists("my_file.txt"):
+  os.remove("my_file.txt")
+else:
+  print("The file does not exist")
+
+#create a new if it doesn't
+my_file = open("my_file.txt", "w")
+my_file.write("Hi! I am Penguin and I am 1 yr old.")
+my_file.close()
+
+#delete file named codingal
+os.remove('Codingal.txt')
+
+#delete the folder
+os.rmdir('Folder')
 
