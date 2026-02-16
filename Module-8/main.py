3f6bcc1b1be1da3f4bf7473ac99da36bef9fc8e5
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 10:01:49 +0600

    Parrot Bird

--- Module-8/main.py
+++ Module-8/main.py
@@ -1,14 +1,21 @@
-class student:
-	grade = 10
-	name = "Penguin"
-	
-	def introduction(self):
-		print("Hi I am a student")
+class Parrot:
 
-	def details(self):
-		print("My name is", self.name)
-		print("I study in Grade", self.grade)
+    # class attribute
+    species = "bird"
 
-ob = student()
-ob.introduction()
-ob.details()
+    # instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# instantiate the Parrot class
+blu = Parrot("Blu", 10)
+woo = Parrot("Woo", 15)
+
+# access the class attributes
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+
+# access the instance attributes
+print("{} is {} years old".format( blu.name, blu.age))
+print("{} is {} years old".format( woo.name, woo.age))
