b1e6e81705bc6e902f75544ff87e86e8c4f68649
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 11:04:57 +0600

    File Handling Operations Part-1

--- Module-9/main.py
+++ Module-9/main.py
@@ -1,33 +1,14 @@
-# Program to copy odd lines of one file to another
-# open file in read mode
-fn = open('Codingal.txt', 'r')
+# write in file using with()function
+with open('Codingal1.txt', 'w') as file:
+  file.write("Hi! I am Penguin and I am 1 yr old.")
+file.close()
 
-# open other file in write mode
-fn1 = open('CodingalUpdated.txt', 'w')
-
-# read the content of the file line by line
-cont = fn.readlines()
-type(cont)
-for i in range(1, len(cont)+1):
-	if(i % 2 != 0):
-		fn1.write(cont[i-1])
-	else:
-		pass
-
-# close the file
-fn1.close()
-
-# open file in read mode
-fn1 = open('CodingalUpdated.txt', 'r')
-
-# read the content of the file
-cont1 = fn1.read()
-
-# print the content of the file
-print(cont1)
-
-# close all files
-fn.close()
-fn1.close()
-
+# split file into words
+with open('Codingal1.txt', 'r') as file:
+  data = file.readlines()
+  print("Words in this file are....")
+  for line in data:
+    word = line.split()
+    print (word)
+file.close()
 
