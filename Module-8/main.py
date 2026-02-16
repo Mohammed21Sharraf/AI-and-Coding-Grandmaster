999cfccbd507072462f44b0c8ea9e6de0f1f7b08
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 10:09:00 +0600

    Animal Class

--- Module-8/main.py
+++ Module-8/main.py
@@ -1,16 +1,43 @@
-class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+# import necessary packages
+from abc import ABC, abstractmethod
+# create a base class
+class Animal(ABC):
 
-  def printname(self):
-    print(self.firstname, self.lastname)
+    # abstract method
+	# should be implemented by all sub-classes
+	def move(self):
+		pass
 
-class Student(Person):
-  def __init__(self, fname, lname, year):
-    super().__init__(fname, lname)
-    self.graduationyear = year
+# sub classes
+class Human(Animal):
 
-x = Student("Joey", "King", 2021)
-x.printname()
-print(x.graduationyear)
+	def move(self):
+		print("I can walk and run")
+
+class Snake(Animal):
+
+	def move(self):
+		print("I can crawl")
+
+class Dog(Animal):
+
+	def move(self):
+		print("I can bark")
+
+class Lion(Animal):
+
+	def move(self):
+		print("I can roar")
+		
+# Driver code
+R = Human()
+R.move()
+
+K = Snake()
+K.move()
+
+R = Dog()
+R.move()
+
+K = Lion()
+K.move()
