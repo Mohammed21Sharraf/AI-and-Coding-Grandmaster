dee569f079b90bec4a845823b2d5ae6f05ef05f0
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-02-16 09:55:14 +0600

    Operations on sets

--- Module-8/main.py
+++ Module-8/main.py
@@ -1,34 +1,15 @@
-# Empty tuple
-my_tuple = ()
-print(my_tuple)
+my_set = {1,2,2,3,4,4,4}
+print("Set :", my_set)
 
-# Tuple having integers
-my_tuple = (1, 2, 3)
-print(my_tuple)
+my_set.add(5)
+print("Updated Set:", my_set)
 
-# tuple with mixed datatypes
-my_tuple = (1, "Hello", 3.4)
-print(my_tuple)
+set1 = my_set
+set2 = {2,4,4,6}
 
-# nested tuple
-my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
-print(my_tuple)
-
-# Accessing tuple elements using indexing
-my_tuple = ('p','e','r','m','i','t')
-print(my_tuple[0])   
-print(my_tuple[5])   
-
-# nested tuple
-n_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
-
-# nested index
-print(n_tuple[0][3])       
-print(n_tuple[1][1])      
-
-# Slicing
-print("Sliced :", my_tuple[1:4])
-
-# Iterating through tuple
-for letter in (my_tuple):
-    print("Hello", letter)
+print("\nSet 1", set1)
+print("Set 2", set2)
+print("Difference")
+print(set1.difference(set2))
+print("Symmeteric Difference")
+print(set1.symmetric_difference(set2))
