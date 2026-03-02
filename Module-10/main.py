97fbdc07b90c95ab21c0d32bcbeda962569f3ae1
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-02 15:50:46 +0600

    Calculate al factors of a given number

--- Module-10/main.py
+++ Module-10/main.py
@@ -1,23 +1,14 @@
-# Program to find if a number is Armstrong number
+# To find factors of user input
  
-# Take input from the user
-number = int(input("Input your number: "))
+# Goes from 1 to number and checks if I divide the number. If yes, it is a factor
+def print_factors(number):
+   print("The factors of",number,"are:")
+   for i in range(1, number + 1):
+       if number % i == 0:
+           print(i)
  
-# Calculate number of digits
-digits = len(str(number))
+# Taking input from the user
+number = int(input("Enter your number to find it's factors: "))
  
-# Initialize result variable
-resultNumber = 0
- 
-# find the sum of the a^digits of each digit
-temp = number
-while temp > 0:
-   digit = temp % 10
-   resultNumber += digit ** digits
-   temp //= 10
- 
-# display the result
-if number == resultNumber:
-   print(number,"is an Armstrong number")
-else:
-   print(number, "is not an Armstrong number")
+# Calling our function
+print_factors(number)
