bb2fb76e4f88775f0bbbacc4cf791bf6f1a98d03
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-02 15:50:07 +0600

    Strong Arms

--- Module-10/main.py
+++ Module-10/main.py
@@ -1,10 +1,23 @@
-def prints(n):
-    if n <= 0:
-        return
-
-    print("Codingal")
-    prints(n // 2)
-    prints(n // 2)
-
-# call the function
-prints(8)
+# Program to find if a number is Armstrong number
+ 
+# Take input from the user
+number = int(input("Input your number: "))
+ 
+# Calculate number of digits
+digits = len(str(number))
+ 
+# Initialize result variable
+resultNumber = 0
+ 
+# find the sum of the a^digits of each digit
+temp = number
+while temp > 0:
+   digit = temp % 10
+   resultNumber += digit ** digits
+   temp //= 10
+ 
+# display the result
+if number == resultNumber:
+   print(number,"is an Armstrong number")
+else:
+   print(number, "is not an Armstrong number")
