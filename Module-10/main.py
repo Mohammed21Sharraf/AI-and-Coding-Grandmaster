7fb3b672877b397272231fccf2247ae1a3189d70
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-02 15:53:40 +0600

    Prime Sieve

--- Module-10/main.py
+++ Module-10/main.py
@@ -1,22 +1,17 @@
-# Program to check if given number is prime or not
+def SieveOfEratosthenes(num):
+    prime = [True for i in range(num+1)]
+    p = 2
+    while (p * p <= num):
+        if (prime[p] == True):
+            for i in range(p * p, num+1, p):
+                prime[i] = False
+        p += 1
 
-from math import sqrt 
+    for p in range(2, num+1):
+        if prime[p]:
+            print(p)
 
-number = int(input("Enter your number : "))
-print("\n")
-
-# If given number is greater than 1
-if number > 1:
- 
-    # check if number is divisible from 2 to number/2
-    for i in range(2, int(sqrt(number))+1):
-        
-        # if divisible by any number it is a non prime number
-        if (number % i) == 0:
-            print(number, "is not a prime number")
-            break
-    else:
-        print(number, "is a prime number")
- 
-else:
-    print(number, "is not a prime number")
+num = int(input("Enter a number"))
+print("Following are the prime numbers smaller")
+print("than or equal to", num)
+SieveOfEratosthenes(num)
