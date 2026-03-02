1e84066cf0d673630d54098fb8720bb8a9f4183f
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-02 15:46:38 +0600

    O(n) Time

--- Module-10/main.py
+++ Module-10/main.py
@@ -1,12 +1,11 @@
-# Program to show Constant Time complexity 
- 
-def printnumber(n):
+def OnTime(n):
   iteration=0
-  print("The number entered by user is ",n)
-  iteration+=1
-  print("Total iterations done by the code is ",iteration,"\n")
+  for i in range(1,n+1):
+    iteration+=1
+  print("When n is ",n," Iterations = ",iteration)
  
+OnTime(10)
+OnTime(20)
+OnTime(42)
  
-printnumber(10)
-printnumber(20)
-print("\n With any 'n' the time taken by our code won't change")
+print("\nWith every 'n' the time taken and iterations will increase")
