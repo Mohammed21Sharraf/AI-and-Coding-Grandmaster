3929254dfec23d677f6b11d0bbabacf66c611a54
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-02 15:47:07 +0600

    O(n^2) Time

--- Module-10/main.py
+++ Module-10/main.py
@@ -1,11 +1,15 @@
-def OnTime(n):
+def ONSquareTime(n):
   iteration=0
-  for i in range(1,n+1):
-    iteration+=1
-  print("When n is ",n," Iterations = ",iteration)
+  for i in range(0,n):
+    for j in range(0,n):
+      print("*", end =" ")
+      iteration+=1
+    print("")
+  print("\nWhen n is ",n," Iterations = ",iteration,"\n")
  
-OnTime(10)
-OnTime(20)
-OnTime(42)
+ONSquareTime(5)
+ONSquareTime(4)
+ONSquareTime(3)
  
-print("\nWith every 'n' the time taken and iterations will increase")
+print("\nWith every 'n' the time taken equals n^2")
+print("O(n^2)")
