b9d498b23e92a21abf4eb249c29b7bf6805b5735
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-02 16:16:42 +0600

    PowerOf4

--- Module-11/main.py
+++ Module-11/main.py
@@ -1,18 +1,26 @@
-# Program to check if a number is the power of 2
-  
-def power2(number):
-    
-    # As the power of 2 will have only 1 set bit, then n-1 & n will always be 0 for any power of 2
-    if (number == 0):
-        return 0
-    if ((number & (~(number - 1))) == number):
-        return 1
-    return 0
-  
-number = int(input("Enter the number : "))
+# Program to check if a number is power of 4
  
-if(power2(number)):
-    print("\nThe number is power of 2")
+def powerOf4(number):
+     
+    count = 0
+     
+    # If only 1 set bit exists
+    if (number & (~(number & (number - 1)))):
+         
+        # Count 0 bits before set bit
+        while(number > 1):
+            number >>= 1
+            count += 1
+         
+        # If count is even return true else false
+        if(count % 2 == 0):
+            return True
+        else:
+            return False
+ 
+ 
+number = int(input("Enter your number : "))
+if(powerOf4(number)):
+    print(number, 'is a power of 4')
 else:
-    print("\nThe number is not power of 2")
- 
+    print(number, 'is not a power of 4')
