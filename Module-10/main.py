bd387ac487570a6b2584fff8975da19014035840
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-02 15:48:08 +0600

    Recursion Time Complexity

--- Module-10/main.py
+++ Module-10/main.py
@@ -1,15 +1,10 @@
-def ONSquareTime(n):
-  iteration=0
-  for i in range(0,n):
-    for j in range(0,n):
-      print("*", end =" ")
-      iteration+=1
-    print("")
-  print("\nWhen n is ",n," Iterations = ",iteration,"\n")
- 
-ONSquareTime(5)
-ONSquareTime(4)
-ONSquareTime(3)
- 
-print("\nWith every 'n' the time taken equals n^2")
-print("O(n^2)")
+def prints(n):
+    if n <= 0:
+        return
+
+    print("Codingal")
+    prints(n // 2)
+    prints(n // 2)
+
+# call the function
+prints(8)
