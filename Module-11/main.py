acfe668dd4fd003d3720eb40f237432894cc2415
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-02 16:16:29 +0600

    PowerOf2

--- Module-11/main.py
+++ Module-11/main.py
@@ -1,38 +1,18 @@
-# Program to find two numbers that are odd occurring 
- 
-def printTwoOdd(arr, size):
-     
-    # xorof2 will hold xor of the 2 odd occurring numbers
-    xorof2 = arr[0]
- 
-    # These will hold 2 odd occurring numbers
-    x = 0
-    y = 0
- 
-    # This will hold the rightmost set bot from xorof2
-    bit = 0
- 
-    for i in range(1, size):
-        xorof2 = xorof2 ^ arr[i]
- 
-    setbit = xorof2 & ~(xorof2 - 1)
+# Program to check if a number is the power of 2
+  
+def power2(number):
     
-    # If number is haivng set bit at location we need then XOR it with x else y
-    for i in range(size):
-        if(arr[i] & setbit):
-            x = x ^ arr[i]
-        else:
-            y = y ^ arr[i]
+    # As the power of 2 will have only 1 set bit, then n-1 & n will always be 0 for any power of 2
+    if (number == 0):
+        return 0
+    if ((number & (~(number - 1))) == number):
+        return 1
+    return 0
+  
+number = int(input("Enter the number : "))
  
-    print("The two ODD elements are", x, "&", y)
+if(power2(number)):
+    print("\nThe number is power of 2")
+else:
+    print("\nThe number is not power of 2")
  
-# Create an empty array
-arr = []
- 
-# Take array size and elements as input
-arr_size = int(input("Enter size of the array : "))
-for i in range(0,arr_size):
-    z = int(input("Enter element : "))
-    arr.append(z)
-    
-printTwoOdd(arr, arr_size)
