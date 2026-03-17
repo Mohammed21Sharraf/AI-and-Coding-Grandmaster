d2e8a623b3f3dc58a662e238fff78090caa81d4e
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-17 16:17:00 +0600

    Let's Analyze Tables

--- Module-13/SQL.py
+++ Module-13/SQL.py
@@ -17,9 +17,15 @@ conn = sqlite3.connect(database)
 print('Opened data successfully')
 
 # Read SQL query for getting all the tables of database into a dataframe
-# Here SELECT * means select all
 import pandas as pd
 tables = pd.read_sql("""SELECT * 
                     FROM sqlite_master
                     WHERE type='table';""", conn)
 tables
+
+# Read Table from the database into dataframe
+matches = pd.read_sql("""SELECT *
+                        FROM Match;""", conn)
+
+# Print Table info
+matches.info()
