9df29ec944377bbc4f5ed969e7a000581a63f5b9
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-17 16:20:55 +0600

    Joins in SQL

--- Module-13/SQL.py
+++ Module-13/SQL.py
@@ -1,48 +1,49 @@
-#Import Dataset
+# Import Dataset
 from google.colab import files
 uploaded = files.upload()
 
-# Connect with sqlite database
-# Import necessary libraries
-import pandas as pd
+# Import Necessary Libraries
 import numpy as np
-
+import pandas as pd
 import sqlite3
 
+# Setup connection with database 
+# And display all tables inside the database
 database = 'database.sqlite'
 
 conn = sqlite3.connect(database)
-print('Opened data successfully')
 
-# Display all the tables of the database
-df = pd.read_sql("""SELECT * 
-                    FROM sqlite_master
-                    WHERE type='table';""", conn)
-df
+tables = pd.read_sql("""SELECT *
+                        FROM sqlite_master
+                        WHERE type='table'""", conn)
 
-# Display the first five rows of the Player_Match table
-player_match = pd.read_sql("""SELECT *
-                        FROM Player_Match""", conn)
+tables
 
-player_match.head()
+# Check how Inner join works
+joined_city = pd.read_sql("""SELECT c.Country_Id, c.Country_Name, ci.City_Name
+                            FROM country c
+                            INNER JOIN city ci
+                            ON c.Country_Id == ci.Country_id""", conn)
+joined_city
 
-# Check the presence of null values in the Player_Match table
-null_player_match = pd.read_sql("""SELECT *
-                        FROM Player_Match
-                        WHERE Team_Id IS NULL""", conn)
+# Check how Outer join works
+joined_left = pd.read_sql("""SELECT *
+                            FROM player
+                            LEFT JOIN season
+                            ON player.Player_Id == season.Man_of_the_Series""", conn)
+joined_left
 
-null_player_match
+# Check how Cross join works
+joined_cross = pd.read_sql("""SELECT c.Country_Id, c.Country_Name, ci.City_Name
+                            FROM country c
+                            CROSS JOIN city ci""", conn)
+joined_cross
 
-# Display the first five rows of the Match table
-toss_dec = pd.read_sql("""SELECT *
-                        FROM Match""", conn)
+# Check how Union Clause works
+union = pd.read_sql("""SELECT Player_Name 
+                      FROM player
+                      UNION
+                      SELECT Team_Name
+                      FROM team""", conn)
 
-toss_dec.head()
-
-# Check the presence of null values in the Match table
-null_match = pd.read_sql("""SELECT *
-                        FROM Match
-                        WHERE MATCH_Winner IS NULL""", conn)
-
-null_match
-
+union
