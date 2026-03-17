fa5c485b5be6a1b39b492dbf467fbb57ef41931e
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-03-17 16:22:19 +0600

    Query Inside Query

--- Module-13/SQL.py
+++ Module-13/SQL.py
@@ -2,13 +2,12 @@
 from google.colab import files
 uplaoded = files.upload()
 
-# Import necessary libraries
+# Import Necessary Libraries
 import pandas as pd
 import numpy as np
-from datetime import datetime
 
-# Setup a connection with database
-# and print all the tables inside it
+# Setup connection with database 
+# And display all tables inside the database
 import sqlite3
 
 database = 'database.sqlite'
@@ -20,12 +19,53 @@ tables = pd.read_sql("""SELECT *
                     WHERE type='table';""", conn)
 tables
 
-# Aliasing 
-match_details = pd.read_sql('''SELECT Season_Id, Match_Id,  
-                              v.Venue_Name, c.City_Name, t.Team_Name AS Winner 
-                              FROM Match
-                              INNER JOIN Venue AS v ON match.Venue_Id == v.Venue_Id
-                              INNER JOIN City AS c ON v.City_Id == c.City_Id
-                              INNER JOIN Team AS t ON match.Match_Winner == t.Team_Id;''', conn)
+# Check the Team_Id of CSK
+team = pd.read_sql("""SELECT * FROM Team""", conn)
 
-match_details
+team
+
+# Check Season_Id for the tournament in year 2015
+season = pd.read_sql("""SELECT * FROM Season""", conn)
+
+season
+
+# Fetch Details of all matches played by CSK in year 2015
+csk_matches_2015 = pd.read_sql("""SELECT Match_Id, Team_2 as Away_Team, Toss_Winner, Match_Winner
+                            FROM Match 
+                            WHERE Team_1 = 
+                            (SELECT Team_1
+                            FROM Match
+                            WHERE Team_1 == 3 AND Season_Id == 8)""", conn)
+
+print("Matches Played By Chennai Super Kings in Year 2015")
+csk_matches_2015
+
+# Fetch Details of all matches won by CSK in year 2015
+csk_wins = pd.read_sql("""SELECT *
+                          FROM Match
+                          WHERE Match_Winner == 3 AND Season_Id == 8""", conn)
+print("Matches won by CSK as Home Team in Year 2015")
+csk_wins
+
+# Fetch details of all the matches 
+# where batsman score more than 5 in year 2015
+match_runs = pd.read_sql("""SELECT Match_Id, Runs_Scored as Total_Runs, Innings_No
+                            FROM Batsman_Scored
+                            WHERE Total_Runs > 5 AND Match_Id IN  
+                            (SELECT Match_Id
+                            FROM Match
+                            WHERE Season_Id == 8)""", conn)
+
+print("Matches with Scored Runs Greater Than 5 in Year 2015")
+match_runs
+
+# Fetch details of Matches played in year 2015
+# where Total Runs Scored were greater than average scored run
+avg_runs = pd.read_sql("""SELECT Match_Id, Runs_Scored as Total_Runs, Innings_No
+                            FROM Batsman_Scored
+                            WHERE Innings_No == 1 AND Runs_Scored > 
+                            (SELECT AVG(Runs_Scored)
+                            FROM Batsman_Scored)""", conn)
+
+print("Matches with Scored Runs Greater Than Average Scored Runs")
+avg_runs
