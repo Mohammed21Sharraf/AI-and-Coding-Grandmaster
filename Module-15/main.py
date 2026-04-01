5fe561c35123a677a6e474fb03d25fecaea86f18
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-04-01 18:23:00 +0600

    Analysis of Blood Sugar Level

--- Module-15/main.py
+++ Module-15/main.py
@@ -1,62 +1,23 @@
-# Import Pandas and Matplotlib
-import pandas as pd
 import matplotlib.pyplot as plt
 
-# Upload File and Make a Copy of the File
-countries_df = pd.read_csv('countries.csv')
-countries = countries_df
-countries.head(3)
+blood_sugar_men = [113, 85, 90, 150, 149, 88, 93, 115, 135, 80, 77, 82, 129]
+blood_sugar_women = [67, 98, 89, 120, 133, 150, 84, 69, 89, 79, 120, 112, 100]
 
-# Extract the rows where the year is 1952
-c_52 = countries.loc[countries['year'] == 1952]
-c_52.head()
+type = [blood_sugar_men, blood_sugar_women]
+colors = ['g', 'r']
+label = ['men', 'women']
+bins = [80, 100, 125, 150]
+plt.xlabel("Blood Sugar Range")
+plt.ylabel("Total no. of patients")
+# Diabetic blood_sugar range
+#
+# 80 - 100 = normal
+# 100 - 125 = pre-diabetic
+# above 125 = diabetic
 
-# Extract the rows where the year is 2007
-c_07 = countries.loc[countries['year'] == 2007]
-c_07.head()
+plt.hist(type, bins=bins, width=0.95, color=colors,
+    label=label, orientation="horizontal")
 
-type(c_52)
-
-# Merge the '52 and the '07 dataframes together
-c_merge = c_52.merge(c_07, left_on='country', right_on='country')
-c_merge.head()
-
-# Drop both the year columns
-c_merge.drop(['year_x', 'year_y'], axis=1)
-c_merge.head()
-
-# Create a new column that takes the difference between the population_y and the population_x column
-c_merge['population_growth'] = c_merge['population_y'] - c_merge['population_x']
-c_merge.head()
-
-# Test the math
-31889923 - 8425333
-
-c_merge.shape, type(c_merge)
-
-# Sort the values so you get back the 10 countries with the biggest population growth
-c_merge = c_merge.sort_values('population_growth', ascending=False).head(10)
-c_merge.head(10)
-
-# Now lets plot our data!
-names = ['China', 'India', 'United States', 'Indonesia', 'Brazil', 'Pakistan', 'Bangladesh', 'Nigeria', 'Mexico', 'Philippines']
-pop_grow = (c_merge['population_growth'] / 10**6)
-
-plt.figure(figsize=(15,9))
-plt.bar(names,pop_grow,width=0.6)
-plt.xlabel('Country')
-plt.ylabel('Population Growth (Millions)')
-plt.title('Top 10 Countries w/the Biggest Population Growth from 1952 to 2007')
-plt.xticks(rotation=45)
-
-# zip joins x and y coordinates in pairs
-for x,y in zip(names,pop_grow):
-    label = "({:.2f}".format(y)
-    
-    plt.annotate(label, # this is the text
-                (x,y), # this is the point to label
-                textcoords="offset points", # how to position the text
-                xytext=(0,10), # distance from text to points (x,y)
-                ha='center') # horizontal alignment can be left, right or center
-
+plt.title("Blood Sugar Level Chart")
+plt.legend()
 plt.show()
