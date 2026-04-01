37c53a838640913459ef6b9ba298f24d28b8bc17
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-04-01 17:35:10 +0600

    Find out students' average and marks using bar graph and line graph

--- Module-14/main.py
+++ Module-14/main.py
@@ -1,12 +1,31 @@
-import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 
-exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
-        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
-        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
-        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
-labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+students_names=["sanjay","Rahul","Karan","Wasim","Ramesh","Ajay","Sartaj","Priya"]
+students_marks=[35,50,20,45,25,40,25,40]
 
-df = pd.DataFrame(exam_data , index=labels)
-print("Summary of the basic information about this DataFrame and its data:")
-print(df.info())
+marks_perc = []
+for x in students_marks:
+	res = (x/50)*100
+	marks_perc.append(res)
+
+print(marks_perc)
+
+# line chart
+def marks_line_chart():
+  plt.plot(students_names,students_marks)
+  plt.title("Students Marks Graph")
+  plt.xlabel("Students Names")
+  plt.ylabel("Students Marks")
+  plt.show()
+
+marks_line_chart()
+
+# bar chart 
+def percentage_bar_chart():
+  plt.bar(students_names,marks_perc)
+  plt.title("Students' Percentage Graph")
+  plt.xlabel("Student Names")
+  plt.ylabel("Student Percentage")
+  plt.show()
+
+percentage_bar_chart()
