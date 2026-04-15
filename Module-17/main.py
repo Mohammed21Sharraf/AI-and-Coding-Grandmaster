c2d485246cb221dd29e5d9f368dc5bc785ec4484
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-04-15 13:32:28 +0600

    Dice Roll

--- Module-17/main.py
+++ Module-17/main.py
@@ -1,11 +1,12 @@
-prob_st = 0.2
+import numpy as np
 
-prob_st_pos = 0.2*0.85
-prob_nst_pos = 0.8*0.02
-prob_positive = prob_st_pos + prob_nst_pos 
+# create 6 sided "die"
+die_sides = int(input("enter number of sides for dice (6/12) : "))
+die = range(1, die_sides)
 
-prob_pos_given_st = 0.85
+# set number of rolls
+num_rolls = int(input("Enter number of times you want to roll the dice : "))
 
-prob_result = (prob_st*prob_pos_given_st)/prob_positive
-
-print("Probability of person testing positive having step throat is :", round((prob_result),3))
+# roll the "die" the set amount of times
+results = np.random.choice(die, size = num_rolls, replace = True)
+print(results)
