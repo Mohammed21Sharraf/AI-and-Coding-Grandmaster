cd1c645b987518eaaf8f160452d02066fa8cd0ab
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-04-15 13:33:22 +0600

    Coin CDF

--- Module-17/main.py
+++ Module-17/main.py
@@ -1,18 +1,6 @@
 import scipy.stats as stats
 
-# value of interest
-# change this
-x = 3
+# calculate the probability
+prob = 1 - stats.binom.cdf(6, 10, 0.5)
 
-# sample size
-# change this
-n = 10
-
-# calculate probability
-prob_1 = stats.binom.pmf(x, n, 0.5)
-print("Probability of getting 3 heads")
-print(prob_1)
-
-prob_2 = 1-stats.binom.pmf(0, n=10, p=.5)-stats.binom.pmf(1, n=10, p=.5)-stats.binom.pmf(2, n=10, p=.5)
-print("Probability of getting more than 2 heads")
-print(prob_2)
+print("the probability of getting more than 6 heads in 10 coin flips is :", prob)
