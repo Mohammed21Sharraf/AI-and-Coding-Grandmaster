7d7f3fca0b31b9f3082965615c563f369b101571
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-04-15 13:35:25 +0600

    Puppies Data Sampling

--- Module-17/main.py
+++ Module-17/main.py
@@ -1,15 +1,50 @@
-import scipy.stats as stats
+import numpy as np
 
-## Checkpoint 1
-# calculate prob_more_than_20
-prob1 = 1-stats.poisson.cdf(20, 15)
+#Below is an array that represents the puppies we have in our sample, where 1 
+#represents the puppies with blue eyes, and 0 represents the puppies with hazel eyes.
 
-# print prob_more_than_20
-print(prob1)
+np.random.seed(42)
 
-## Checkpoint 2
-# calculate prob_17_to_21
-prob2 = stats.poisson.cdf(21, 15) - stats.poisson.cdf(16, 15)
+puppies = np.array([1,0,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1])
 
-# print prob_17_to_21
-print(prob2)
+#Finding the proportion of puppies with blue eyes in the above array. 
+#Storing this value in a variable p
+
+p=puppies.mean()
+print("Mean", p)
+print("Standard Deviation", puppies.std())
+print("Variance", puppies.var())
+
+#Using numpy's random.choice to simulate 5 draws from the puppies array.
+np.random.choice(puppies, size=(1,5), replace=True)
+np.random.choice(puppies, size=(1,5), replace=True).mean()
+
+#Proportion of sample with blue eyes in 0.8
+
+#Repeating the above to obtain 10,000 additional proportions, where 
+#each sample was of size 5. Storing these in a variable called sample_props
+print("\nSampling Distribution with size 5 \n")
+sample_props= []
+for i in range(10000):
+    sample = np.random.choice(puppies, 5, replace=True)
+    sample_props.append(sample.mean())
+sample_props = np.array(sample_props)
+
+#The mean of the sampling distribution
+sm = sample_props.mean()
+print("Mean", sample_props.mean())
+print("Standard Deviation", sample_props.std())
+print("Variance", sample_props.var())
+
+print("\nSampling Distribution with size 20 \n")
+
+#Let's repeat all these steps with a new sample, this time size 20.
+twenty_sample_props= []
+for i in range(10000):
+    sample = np.random.choice(puppies, 20, replace=True)
+    twenty_sample_props.append(sample.mean())
+twenty_sample_props = np.array(twenty_sample_props)
+
+print("New Mean", twenty_sample_props.mean())
+print("New Standard Deviation", twenty_sample_props.std())
+print("New Variance", twenty_sample_props.var())
