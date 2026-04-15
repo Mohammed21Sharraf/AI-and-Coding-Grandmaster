1f85d14607660d99c6a497ec5d1559ba14a343d4
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-04-15 13:33:51 +0600

    Rain Expectation

--- Module-17/main.py
+++ Module-17/main.py
@@ -1,6 +1,9 @@
 import scipy.stats as stats
 
-# calculate the probability
-prob = 1 - stats.binom.cdf(6, 10, 0.5)
+# expected value = 10, probability of observing 6
+prob1 = stats.poisson.pmf(6, 10)
+print("probability of raining for exactly 6 days :", prob1)
 
-print("the probability of getting more than 6 heads in 10 coin flips is :", prob)
+# expected value = 10, probability of observing 12-14
+prob2 = stats.poisson.pmf(12, 10) + stats.poisson.pmf(13, 10) + stats.poisson.pmf(14, 10)
+print("probability of raining for 12-14 days :", prob2)
