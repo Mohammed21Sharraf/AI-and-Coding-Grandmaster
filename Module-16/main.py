fc33eecfa15292bae01d140297a63f299ad298ea
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-04-15 13:04:04 +0600

    Titanic Survival Prediction-I

--- Module-16/main.py
+++ Module-16/main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 """####**Import Dataset**"""
+
 # Import dataset
 data = pd.read_csv('Titanic Dataset.csv')
 
@@ -18,49 +19,13 @@ data.isnull().sum()
 
 """**Null values present in Cabin -**
 
-#### **Quartiles of Feature Age**
+#### **Boxplot of Feature Age and Pclass**
 """
 
-age_q1 = np.quantile(data['Age'], 0.25)
-age_q2 = np.quantile(data['Age'], 0.50)
-age_q3 = np.quantile(data['Age'], 0.75)
+plt.boxplot(data['Age'])
+plt.title('Age distribution')
+plt.show()
 
-print("Age Quartiles -")
-print("Q1 -", age_q1)
-print("Q2 -", age_q2)
-print("Q3 -", age_q3)
-
-"""#### **Interquartile Range of Feature Age**"""
-
-IQR_age = age_q3 - age_q1
-print("Interquartile Range :", IQR_age)
-
-"""#### **Plot Histogram for feature Age**"""
-
-plt.hist(data['Age'])
-plt.ylabel("Count of Passengers")
-plt.xlabel("Age")
-
-"""#### **Quartiles for feature Fare**"""
-
-fare_q1 = np.quantile(data['Fare'], 0.25)
-fare_q2 = np.quantile(data['Fare'], 0.50)
-fare_q3 = np.quantile(data['Fare'], 0.75)
-
-print("Fare Quartiles -")
-print("Q1 -", fare_q1)
-print("Q2 -", fare_q2)
-print("Q3 -", fare_q3)
-
-"""#### **Interquartile Range of Feature Fare**"""
-
-IQR_fare = fare_q3 - fare_q1
-print("Interquartile Range :", IQR_fare)
-
-"""#### **Plot histogram for feature Fare**"""
-
-bins =np.arange(0,250,20)
-plt.hist(data['Fare'], bins=np.arange(1,250, 20))
-plt.ylabel("Count of Passengers")
-plt.xlabel("Fare")
-plt.xticks(bins)
+plt.boxplot(data['Pclass'])
+plt.title('Passenger Class distribution')
+plt.show()
