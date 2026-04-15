9268d8ec8a7e23502e775df86649f237d86e7b37
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-04-15 13:10:31 +0600

    Titanic Survival Prediction

--- Module-16/main.py
+++ Module-16/main.py
@@ -8,44 +8,49 @@ import seaborn as sns
 
 """####**Import Dataset**"""
 
+
 # Import dataset
-data = pd.read_csv('Titanic Dataset.csv')
+data = pd.read_csv('Iris Dataset.csv')
 
 data.head(5)
 
-"""#### **Passengers belonging from which gender survived the most**"""
+"""#### **Check Null Values**"""
 
-sns.countplot(data['Gender'], hue=data['Survived'])
+data.isnull().sum()
 
-"""#### **Passengers belonging from which PClass survived the most and the least**"""
+"""- No null values are present
 
-sns.countplot(data['Pclass'], hue=data['Survived'])
+#### **Statistical Information of features**
+"""
 
-"""#### **Highest number of passengers belong to which Age**"""
+data.describe()
 
-sns.distplot(data['Age'],kde=False,bins=40)
+"""#### **CreateBoxplot of all the features and check if outliers are present**"""
 
-"""#### **Highest number of passengers belong to which Gender**"""
+labels = ['Id','SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
+for label in labels:
+  print('Distribution of', label)
+  sns.boxplot(data[label])
+  plt.show()
 
-sns.countplot(data['Gender'])
+"""**Conclusion**
 
-"""#### **Is SibSp correlated/associated with Survived feature**"""
+- SepalWidthCm has outliers
 
-sns.countplot(x='Survived', hue='SibSp', data=data, palette="mako")
-
-"""#### **Is Parch correlated/associated with Survived feature**"""
-
-sns.countplot(x='Survived', hue='Parch', data=data, palette="mako")
-
-"""#### **Is the feature Fare having normal distribution/spread of data**"""
-
-sns.distplot(data['Fare'])
-plt.show()
-
-"""#### **Check the age group of majority of people belonging to PClass=1**"""
-
-sns.boxplot(x='Pclass',y='Age',data=data,palette='winter')
-
-"""#### **Check the correlation of all the features with target variable ‘Survived’**"""
+#### **Check correlation between all the features**
+"""
 
 sns.heatmap(data.corr())
+
+"""#### **Check if any feature is skewed or not**"""
+
+labels = ['Id','SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
+for label in labels:
+  print('Distribution of', label)
+  sns.distplot(data[label])
+  plt.show()
+
+labels = ['Id','SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
+for label in labels:
+  print('skewness of ', label)
+  print(data[label].skew())
