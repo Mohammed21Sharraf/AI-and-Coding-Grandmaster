e3394d9c6e64ea4cc8a725706b5abec743f411ae
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-09-03 17:34:30 +0600

    Gradient Descent Function

--- Module-17/main.py
+++ Module-17/main.py
@@ -1,47 +1,53 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import datasets, linear_model
+from numpy import asarray
+from numpy import arange
+from numpy.random import rand
+from matplotlib import pyplot
 
-#Importing a library for Errors of all outputs are averaged with uniform weight.
-from sklearn.metrics import mean_squared_error, r2_score
+# objective function
+def objective(x):
+    return x**2.0
+ 
+# derivative of objective function
+def derivative(x):
+    return x * 2.0
+ 
+# gradient descent algorithm
+def gradient_descent(objective, derivative, bounds, n_iter, step_size):
+    # track all solutions
+    solutions, scores = list(), list()
+    # generate an initial point
+    solution = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+    # run the gradient descent
+    for i in range(n_iter):
+        # calculate gradient
+        gradient = derivative(solution)
+        # take a step
+        solution = solution - step_size * gradient
+        # evaluate candidate point
+        solution_eval = objective(solution)
+        # store solution
+        solutions.append(solution)
+        scores.append(solution_eval)
+        # report progress
+        print('>%d f(%s) = %.5f' % (i, solution, solution_eval))
+    return [solutions, scores]
+ 
+# define range for input
+bounds = asarray([[-1.0, 1.0]])
+# define the total iterations
+n_iter = 30
+# define the step size
+step_size = 0.1
 
-# Load the diabetes dataset
-diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
-
-# Use only one feature
-diabetes_X = diabetes_X[:, np.newaxis, 2]
-
-# Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
-
-# Split the targets into training/testing sets
-diabetes_y_train = diabetes_y[:-20]
-diabetes_y_test = diabetes_y[-20:]
-
-# Create linear regression object
-regr = linear_model.LinearRegression()
-
-# Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
-
-# Make predictions using the testing set
-diabetes_y_pred = regr.predict(diabetes_X_test)
-
-# The coefficients
-print('Coefficients: \n', regr.coef_)
-# The mean squared error
-print('Mean squared error: %.2f'
-#       % mean_squared_error(diabetes_y_test, diabetes_y_pred))
-# The coefficient of determination: 1 is perfect prediction
-print('Coefficient of determination: %.2f'
-#       % r2_score(diabetes_y_test, diabetes_y_pred))
-
-# Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
-
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
+# perform the gradient descent search
+solutions, scores = gradient_descent(objective, derivative, bounds, n_iter, step_size)
+# sample input range uniformly at 0.1 increments
+inputs = arange(bounds[0,0], bounds[0,1]+0.1, 0.1)
+# compute targets
+results = objective(inputs)
+# create a line plot of input vs result
+pyplot.plot(inputs, results)
+# plot the solutions found
+pyplot.plot(solutions, scores, '.-', color='red')
+# show the plot
+pyplot.show()
