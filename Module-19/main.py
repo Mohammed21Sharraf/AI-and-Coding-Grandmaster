bd66e0bdefeb83243cd50582e6f38dc1e8530083
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-09-21 12:41:37 +0600

    Apply the binary class algorithm to the given datase

--- Module-19/main.py
+++ Module-19/main.py
@@ -1,40 +1,74 @@
-from sklearn.datasets import make_classification
-from matplotlib import pyplot as plt
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+# Commented out IPython magic to ensure Python compatibility.
+#Importing requirede libraries
+
 import pandas as pd
+from matplotlib import pyplot as plt
+from google.colab import files
+# %matplotlib inline
 
-# Generate and dataset for Logistic Regression
-x, y = make_classification(
-    n_samples=100,
-    n_features=1,
-    n_classes=2,
-    n_clusters_per_class=1,
-    flip_y=0.03,
-    n_informative=1,
-    n_redundant=0,
-    n_repeated=0
-)
+#Importing Dataset required
 
-# Create a scatter plot
-plt.scatter(x, y, c=y, cmap='rainbow')
-plt.title('Scatter Plot of Logistic Regression')
-plt.show()
+uploaded = files.upload()
+df = pd.read_csv("insurance_data.csv")
+df.head()
 
-# Split the dataset into training and test dataset
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+#Ploting the dataset
 
-# Create a Logistic Regression Object, perform Logistic Regression
-log_reg = LogisticRegression()
-log_reg.fit(x_train, y_train)
+plt.scatter(df.age,df.bought_insurance,marker='+',color='red')
 
-# Show to Coeficient and Intercept
-print(log_reg.coef_)
-print(log_reg.intercept_)
+#Spliting the training Values usijg sklearn
 
-# Perform prediction using the test dataset
-y_pred = log_reg.predict(x_test)
+from sklearn.model_selection import train_test_split
 
-print(y_pred)
+#Defining the Training and Testing Variables
+
+X_train, X_test, y_train, y_test = train_test_split(df[['age']],df.bought_insurance,train_size=0.8)
+
+X_test
+
+#Using Logistic Regression Algorithmn
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+
+#Fitting the model
+
+model.fit(X_train, y_train)
+
+X_test
+
+#Predicting the Y
+
+y_predicted = model.predict(X_test)
+
+model.predict_proba(X_test)
+
+#Testing model
+
+model.score(X_test,y_test)
+
+y_predicted
+
+X_test
+
+model.coef_
+
+model.intercept_
+
+import math
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
+def prediction_function(age):
+    z = 0.042 * age - 1.53 # 0.04150133 ~ 0.042 and -1.52726963 ~ -1.53
+    y = sigmoid(z)
+    return y
+
+age = 35
+prediction_function(age)
+
+age = 43
+prediction_function(age)
+
+#0.485 is more than 0.5 which means person with 43 will buy the insurance
 
