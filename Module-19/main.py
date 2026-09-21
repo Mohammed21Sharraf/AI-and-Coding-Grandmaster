c727a092cd8a2415109e5321d6f334ac944ce701
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-09-21 12:39:08 +0600

    Max margin separating hyperlink

--- Module-19/main.py
+++ Module-19/main.py
@@ -1,22 +1,38 @@
+#Maximum margin separating hyperplane
+
+print(__doc__)
+
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_breast_cancer
+from sklearn.linear_model import SGDClassifier
+from sklearn.datasets import make_blobs
 
-dataset = load_breast_cancer()
+# we create 50 separable points
+X, Y = make_blobs(n_samples=50, centers=2, random_state=0, cluster_std=0.60)
 
-sns.set_style('dark')
-import matplotlib as mpl
-mpl.style.use(['https://gist.githubusercontent.com/BrendanMartin/01e71bb9550774e2ccff3af7574c0020/raw/6fa9681c7d0232d34c9271de9be150e584e606fe/lds_default.mplstyle'])
-mpl.rcParams.update({"figure.figsize": (8,6), "axes.titlepad": 22.0})
+# fit the model
+clf = SGDClassifier(loss="hinge", alpha=0.01, max_iter=200)
 
-print('Target variables  : ', dataset['target_names'])
+clf.fit(X, Y)
 
-(unique, counts) = np.unique(dataset['target'], return_counts=True)
+# plot the line, the points, and the nearest vectors to the plane
+xx = np.linspace(-1, 5, 10)
+yy = np.linspace(-1, 5, 10)
 
-print('Unique values of the target variable', unique)
-print('Counts of the target variable :', counts)
+X1, X2 = np.meshgrid(xx, yy)
+Z = np.empty(X1.shape)
+for (i, j), val in np.ndenumerate(X1):
+    x1 = val
+    x2 = X2[i, j]
+    p = clf.decision_function([[x1, x2]])
+    Z[i, j] = p[0]
+levels = [-1.0, 0.0, 1.0]
+linestyles = ['dashed', 'solid', 'dashed']
+colors = 'k'
+plt.contour(X1, X2, Z, levels, colors=colors, linestyles=linestyles)
+plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired,
+            edgecolor='black', s=20)
 
-sns.barplot(x=dataset['target_names'], y=counts)
-plt.title('Target variable counts in dataset')
+plt.axis('tight')
 plt.show()
+
